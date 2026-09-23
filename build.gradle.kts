d946aebe81699df58110d05601248f3e0cf4ee23
MatheussRezende <matheus.raimundo@germinare.org.br>
2026-09-23 09:26:04 -0300

    chore(sonar): adiciona configuração do sonar ao gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.gms.google.services) apply false
+    id("org.sonarqube") version "7.5.0.8588"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Olius-Inter_Olius_Mobile")
+        property("sonar.organization", "olius-inter")
+    }
 }
